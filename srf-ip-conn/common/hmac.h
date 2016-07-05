c0429c10b37720d67ed10c52e169a013fbd2e961
Nonoo <nonoo@nonoo.hu>
2016-07-05 14:17:55 +0200

    Add closing zero for password string

--- srf-ip-conn/common/hmac.h
+++ srf-ip-conn/common/hmac.h
@@ -4,7 +4,7 @@
 #include "srf-ip-conn/common/srf-ip-conn-packets.h"
 #include "srf-ip-conn/common/types.h"
 
-void hmac_add(uint8_t token[SRF_IP_CONN_TOKEN_LENGTH], char password[SRF_IP_CONN_MAX_PASSWORD_LENGTH], srf_ip_conn_packet_t *packet, uint16_t payload_length);
-flag_t hmac_check(uint8_t token[SRF_IP_CONN_TOKEN_LENGTH], char password[SRF_IP_CONN_MAX_PASSWORD_LENGTH], srf_ip_conn_packet_t *packet, uint16_t payload_length);
+void hmac_add(uint8_t token[SRF_IP_CONN_TOKEN_LENGTH], char password[SRF_IP_CONN_MAX_PASSWORD_LENGTH+1], srf_ip_conn_packet_t *packet, uint16_t payload_length);
+flag_t hmac_check(uint8_t token[SRF_IP_CONN_TOKEN_LENGTH], char password[SRF_IP_CONN_MAX_PASSWORD_LENGTH+1], srf_ip_conn_packet_t *packet, uint16_t payload_length);
 
 #endif
